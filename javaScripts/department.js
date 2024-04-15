8b6d61cd639023428b70fea2dcd65b22b910ab40
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2024-04-15 12:10:37 +0530

    modification in dept navbar

---
+++ javaScripts/department.js
@@ -0,0 +1,15 @@
+let hambergMenu = document.querySelector('.dept-hamberg-menu');
+let body = document.querySelector('body');
+let nav = document.querySelector('nav');
+let goback = document.querySelector('.goback');
+goback.addEventListener('click', () => {
+    nav.style.right = "-100%";
+})
+hambergMenu.addEventListener('click', e => {
+    nav.style.right = "0%"
+})
+window.addEventListener('click', e => {
+    if (e.x < screen.width - 285) {
+        nav.style.right = "-100%"
+    }
+})
