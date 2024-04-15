e5b99a7c45b2e25b5555bf6e1ae875428e748499
Ranit Jana <ranitjana100@gmail.com>
2024-04-15 18:15:36 +0530

    feat:fetch from file

--- javaScripts/home.js
+++ javaScripts/home.js
@@ -1,10 +1,48 @@
 //bulletin section
-
-let eventInfo = ['../assets/bulletinInfo/1.txt'],
-    eventImg = ['../assets/bulletinImage/1.png']
+let firstEle = document.querySelector('#bulletin');
+let eventInfo;
+const creation = function (text) {
+    firstEle.innerHTML = `
+    <div class="newsBox">
+    <a href="">
+    ${text}
+    </a>
+    </div>
+    `
+};
 
 let idx = 0;
+let refInterval;
+async function getData() {
+    let res = await fetch('../assets/bulletinInfo/bulletinInfo.txt');
+    let text = (await res.text()).split('<##next##>');
+    eventInfo = text;
+}
+getData().then(() => {
+    creation(eventInfo[idx]);
+    idx++;
+})
 
-// setInterval(()=>{
+let calInterval = () => {
+    refInterval = setInterval(() => {
+        creation(eventInfo[idx++])
+        if (idx >= eventInfo.length) idx = 0;
+    }, 3000);
+}
+calInterval();
+let pArrow = document.querySelector('.prev-arrow'), nArrow = document.querySelector('.next-arrow');
 
-// },3000);
+nArrow.addEventListener('click', e => {
+    idx++;
+    if (idx >= eventInfo.length) idx = 0;
+    creation(eventInfo[idx]);
+    clearInterval(refInterval);
+    calInterval();
+})
+pArrow.addEventListener('click', e => {
+    idx--;
+    if (idx < 0) idx = eventInfo.length - 1;
+    creation(eventInfo[idx]);
+    clearInterval(refInterval);
+    calInterval();
+})
