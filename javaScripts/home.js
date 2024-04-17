c44717549b39654d9a563c72b2ff9c147d92b549
Ranit Jana <ranitjana100@gmail.com>
2024-04-17 13:04:08 +0530

    feat:animation

--- javaScripts/home.js
+++ javaScripts/home.js
@@ -1,11 +1,14 @@
 let first = document.querySelector('.first'),
     newsBox = document.querySelector('.newsBox'),
     blackCover = document.querySelector('.black-cover'),
+    loadingPage = document.querySelector('.loadingPage'),
+    welcomeAotImg = document.querySelector('.loadingPage img'),
     eventInfo,
     idx = 0,
     idxImg = 5,
     refInterval,
     images = ['../assets/bulletinImage/first.webp', '../assets/bulletinImage/second.webp', '../assets/bulletinImage/third.webp', '../assets/bulletinImage/fourth.webp', '../assets/bulletinImage/fifth.webp', '../assets/bulletinImage/sixth.webp', '../assets/bulletinImage/seventh.webp', '../assets/bulletinImage/eighth.webp', '../assets/bulletinImage/ninth.webp'];
+
 function imageSlider() {
     // // bulletin section
     //functions
@@ -96,5 +99,25 @@ function announceSeeMore() {
         announcementOverflow.style.overflow = "auto";
     })
 }
-imageSlider();
-announceSeeMore();
+body.style.overflow = "hidden";
+if (!sessionStorage.getItem('loadingPage')) {
+    sessionStorage.setItem('loadingPage', 'true');
+    setTimeout(() => {
+        loadingPage.style.scale = "40";
+        welcomeAotImg.style.filter = "invert(100%) opacity(0%)";
+        loadingPage.style.backgroundColor = "rgb(0,0,0,0)";
+        setTimeout(() => {
+            loadingPage.style.zIndex = "-10";
+            body.style.overflow = "auto";
+        }, 1000);
+        imageSlider();
+        announceSeeMore();
+    }, 2000);
+}
+else {
+    body.style.overflow = "auto";
+    loadingPage.style.display = "none";
+    sessionStorage.removeItem('loadingPage');
+    imageSlider();
+    announceSeeMore();
+}
