ac564367ecae98bdfc381f3ad6cb7fdf56791867
Ranit Jana <ranitjana100@gmail.com>
2024-04-21 18:32:35 +0530

    fix: nav issues

--- javaScripts/header.js
+++ javaScripts/header.js
@@ -20,29 +20,6 @@ let parentNavChild2 = document.querySelectorAll('.parent-nav2>ul');
 
 let allchild = document.querySelectorAll('.parent-nav > ul> li');
 
-parentNav.forEach((val, idx) => {
-    val.addEventListener('click', () => {
-        let arrow = null;
-        try {
-            arrow = val.childNodes[1].childNodes[3];
-            console.log(val.childNodes);
-        }
-        catch (err) {
-            console.log(err);
-        }
-        let nodeVal = parentNavChild[idx].style.display;
-        if (nodeVal == 'block') {
-            parentNavChild[idx].style.display = "none";
-            parentNavChild[idx].style.zIndex = '50';
-            if (arrow) arrow.style.transform = "rotate(90deg)";
-        }
-        else {
-            parentNavChild[idx].style.display = "block";
-            parentNavChild[idx].style.zIndex = '100';
-            if (arrow) arrow.style.transform = "rotate(-90deg)";
-        }
-    }, false)
-})
 function editRoateImg(allchilds) {
     allchild.forEach(val => {
         if (val.getAttribute('class') === 'parent-nav2') {
@@ -56,17 +33,52 @@ function editRoateImg(allchilds) {
         }
     })
 }
+
+parentNav.forEach((val, idx) => {
+    val.addEventListener('click', () => {
+        let arrow = null;
+        try {
+            arrow = val.childNodes[1].childNodes[3];
+        }
+        catch (err) {
+            console.log(err);
+        }
+        parentNavChild.forEach((val, index) => {
+            if (idx != index) {
+                try {
+                    if (val.style.display == 'block') {
+                        val.style.display = "none";
+                        val.style.zIndex = '100';
+                    }
+                }
+                catch (err) {
+                    console.log(err);
+                }
+            }
+        })
+        let nodeVal = parentNavChild[idx].style.display;
+        if (nodeVal == 'block') {
+            parentNavChild[idx].style.display = "none";
+            parentNavChild[idx].style.zIndex = '50';
+            if (arrow) arrow.style.transform = "rotate(90deg)";
+        }
+        else {
+            parentNavChild[idx].style.display = "block";
+            parentNavChild[idx].style.zIndex = '100';
+            if (arrow) arrow.style.transform = "rotate(-90deg)";
+        }
+    }, false)
+})
+
 allchild.forEach((allchilds, idx) => {
     allchilds.addEventListener('click', () => {
         let arrow = null;
         try {
             arrow = allchilds.childNodes[1].childNodes[3];
-            editRoateImg();
         }
         catch (err) {
             console.log(err);
         }
-        console.log(parentNavChild2);
         parentNavChild2.forEach((val, index) => {
             if (idx - 1 != index) {
                 try {
@@ -81,35 +93,38 @@ allchild.forEach((allchilds, idx) => {
             }
         })
         editRoateImg();
-        try {
-            let nodeVal = parentNavChild2[idx - 1].style.display;
-            if (nodeVal == 'block') {
-                parentNavChild2[idx - 1].style.display = "none";
-                parentNavChild2[idx - 1].style.zIndex = '100';
-                if (arrow) {
-                    if (screen.width <= 571) {
-                        arrow.style.transform = "rotate(90deg)";
+        if (allchilds.getAttribute("class") == 'parent-nav2') {
+            let values = allchilds.childNodes;
+            try {
+                let nodeVal = values[3].style.display;
+                if (nodeVal == 'block') {
+                    values[3].style.display = "none";
+                    values[3].style.zIndex = '100';
+                    if (arrow) {
+                        if (screen.width <= 571) {
+                            arrow.style.transform = "rotate(90deg)";
+                        }
+                        else {
+                            arrow.style.transform = "rotate(0deg)";
+                        }
                     }
-                    else {
-                        arrow.style.transform = "rotate(0deg)";
+                }
+                else {
+                    values[3].style.display = "block";
+                    values[3].style.zIndex = '100';
+                    if (arrow) {
+                        if (screen.width <= 571) {
+                            arrow.style.transform = "rotate(-90deg)";
+                        }
+                        else {
+                            arrow.style.transform = "rotate(-180deg)";
+                        }
                     }
                 }
             }
-            else {
-                parentNavChild2[idx - 1].style.display = "block";
-                parentNavChild2[idx - 1].style.zIndex = '100';
-                if (arrow) {
-                    if (screen.width <= 571) {
-                        arrow.style.transform = "rotate(-90deg)";
-                    }
-                    else {
-                        arrow.style.transform = "rotate(-180deg)";
-                    }
-                }
+            catch (err) {
+                console.log(err);
             }
-        }
-        catch (err) {
-            console.log(err);
         }
         event.stopPropagation();
     }, false)
