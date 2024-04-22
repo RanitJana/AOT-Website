12af8d2ca37b48ef59122fbb4c94ac190c9dcd14
Ranit Jana <ranitjana100@gmail.com>
2024-04-22 14:53:27 +0530

    fix:margin and animation

--- javaScripts/home.js
+++ javaScripts/home.js
@@ -19,10 +19,10 @@ let bigImages = ['./assets/bulletinImage/firstBig.webp', './assets/bulletinImage
 function isInViewport(element) {
     var rect = element.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top >= -1 &&
+        rect.left >= -1 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 1 &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) + 1
     );
 }
 
