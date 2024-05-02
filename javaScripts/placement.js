5cd8a8940cfa756904c79c4d4d6ef3a813fb6f0c
Ranit Jana <ranitjana100@gmail.com>
2024-05-02 20:43:07 +0530

    add company logo

--- javaScripts/placement.js
+++ javaScripts/placement.js
@@ -207,7 +207,7 @@ let images = document.querySelectorAll('.company .name');
 let showMore = document.querySelector('.company .box');
 let showMoreTEXT = document.querySelector('.company .box span');
 let showMoreIMG = document.querySelector('.company .box img');
-window.addEventListener('resize', e => {
+window.addEventListener('load', e => {
     if (screen.width < 918) {
         images.forEach((val, idx) => {
             if (idx > 5)
