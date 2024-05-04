2bb0aa5770b5732492f6807a7268ba37f6d6fee9
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2024-05-05 01:24:30 +0530

    dept page club: hover to pause sliding

--- javaScripts/department.js
+++ javaScripts/department.js
@@ -23,3 +23,51 @@ searches.forEach(search => {
         window.location.href = "./search.html";
     })
 })
+
+
+// =============================== club details sliding logic  ==============================
+var swiper;
+
+        function autosliding() {
+            swiper = new Swiper(".my_Swiper", {
+                slidesPerView: 3,
+                loop: true,
+                spaceBetween: 30,
+                speed: 1000,
+                autoplay: {
+                    delay: 2000,
+                },
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                breakpoints: {
+                    300: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    750: {
+                        slidesPerView: 2,
+                        spaceBetween: 40,
+                    },
+                    1246: {
+                        slidesPerView: 3,
+                        spaceBetween: 50,
+                    },
+                },
+            });
+
+        }
+        autosliding();
+        
+        document.querySelector('.my_Swiper').addEventListener('mouseenter', () => {
+            swiper.autoplay.stop();
+        });
+
+        document.querySelector('.my_Swiper').addEventListener('mouseleave', () => {
+            swiper.autoplay.start();
+        });
