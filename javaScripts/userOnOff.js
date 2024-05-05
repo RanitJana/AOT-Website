d8eb0bf1fd6583218038708dd330f693b3a7e411
Ranit Jana <ranitjana100@gmail.com>
2024-05-06 00:30:36 +0530

    feat:check user on of off

---
+++ javaScripts/userOnOff.js
@@ -0,0 +1,26 @@
+let box = document.querySelector('.onOrOff');
+let p = document.querySelector('.onOrOff p');
+window.addEventListener('load', function (e) {
+    if (!navigator.onLine) {
+        box.style.transform = "translate(-50%,20%)";
+        p.innerHTML = "Poor Connectinon!!";
+        setTimeout(() => {
+            box.style.transform = "translate(-50%,-120%)";
+        }, 3000);
+    }
+}, false);
+window.addEventListener('online', function (e) {
+    p.innerHTML = "Welcome Back !!";
+    box.style.transform = "translate(-50%,20%)";
+    setTimeout(() => {
+        box.style.transform = "translate(-50%,-120%)";
+    }, 3000);
+}, false);
+
+window.addEventListener('offline', function (e) {
+    box.style.transform = "translate(-50%,20%)";
+    p.innerHTML = "Poor Connectinon!!";
+    setTimeout(() => {
+        box.style.transform = "translate(-50%,-120%)";
+    }, 3000);
+}, false);
