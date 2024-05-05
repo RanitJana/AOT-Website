b0e5ec47974c1da8418733a557c5975ab909bc19
Ranit Jana <ranitjana100@gmail.com>
2024-05-06 00:50:47 +0530

    feat:check user on of off

--- javaScripts/userOnOff.js
+++ javaScripts/userOnOff.js
@@ -1,15 +1,20 @@
 let box = document.querySelector('.onOrOff');
 let p = document.querySelector('.onOrOff p');
+let boxImg = document.querySelector('.onOrOff img');
 window.addEventListener('load', function (e) {
     if (!navigator.onLine) {
-        box.style.transform = "translate(-50%,20%)";
+        box.style.backgroundColor = "rgb(255, 106, 106)";
+        boxImg.setAttribute('src', "./assets/images/icons8-no-connection.gif")
         p.innerHTML = "Poor Connection!!";
+        box.style.transform = "translate(-50%,20%)";
         setTimeout(() => {
             box.style.transform = "translate(-50%,-120%)";
         }, 3000);
     }
 }, false);
 window.addEventListener('online', function (e) {
+    box.style.backgroundColor = "rgb(81, 254, 81)";
+    boxImg.setAttribute('src', "./assets/images/icons8-online.gif")
     p.innerHTML = "Welcome Back !!";
     box.style.transform = "translate(-50%,20%)";
     setTimeout(() => {
@@ -18,8 +23,10 @@ window.addEventListener('online', function (e) {
 }, false);
 
 window.addEventListener('offline', function (e) {
-    box.style.transform = "translate(-50%,20%)";
+    box.style.backgroundColor = "rgb(255, 106, 106)";
+    boxImg.setAttribute('src', "./assets/images/icons8-no-connection.gif")
     p.innerHTML = "Poor Connection!!";
+    box.style.transform = "translate(-50%,20%)";
     setTimeout(() => {
         box.style.transform = "translate(-50%,-120%)";
     }, 3000);
