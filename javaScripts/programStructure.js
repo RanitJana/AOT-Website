b8b3d09174ca0388f4a671c0c7340fce33c98b8f
mdsalmanfarsee <far90986@gmail.com>
2024-05-07 20:54:34 +0530

    menu

--- javaScripts/programStructure.js
+++ javaScripts/programStructure.js
@@ -2,8 +2,14 @@ const icon = document.querySelector('.scrollTo');
 window.addEventListener('scroll', () => {
     if (window.scrollY > window.innerHeight) {
         icon.style.display = "block";
+        setTimeout(() => {
+            icon.style.transform = "scale(1)";
+        }, 300);
     }
     else {
-        icon.style.display = "none";
+        icon.style.transform = "scale(0)";
+        setTimeout(() => {
+            icon.style.display = "none";
+        }, 300);
     }
 })
