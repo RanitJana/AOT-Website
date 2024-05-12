b1e5f0157beaaada4e5ddcf48fabdc336b07f848
Ranit Jana <ranitjana100@gmail.com>
2024-05-12 20:57:00 +0530

    add some keywords, fix some nav issues, udit search icons

--- javaScripts/search.js
+++ javaScripts/search.js
@@ -1,6 +1,10 @@
-let sessionKey = sessionStorage.getItem('res').toLowerCase().trim();
+let userInp = sessionStorage.getItem('res');
+let sessionKey = sessionStorage.getItem('res').toLowerCase().trim().split(' ');
+
 let main = document.querySelector('.main');
 let res = document.querySelector('#res');
+
+//create new Nodes on success
 function createSuccessNode(objKey, objLink, objContent) {
     let newNode = document.createElement('div');
     newNode.classList.add('card');
@@ -11,21 +15,27 @@ function createSuccessNode(objKey, objLink, objContent) {
     `;
     main.appendChild(newNode);
 }
+
+//create new Nodes of failure
 function createFailedNode() {
     let newNode = document.createElement('div');
     newNode.classList.add('fail');
     newNode.innerHTML =
         `
             <span>No Result Is Found For: </span>
-            <span>${sessionKey ? sessionKey : 'EMPTY!!'}</span>
+            <span>${userInp ? userInp : 'EMPTY!!'}</span>
     `;
     main.appendChild(newNode);
 }
+
+// utility to find all the keys relevant to user input
 function searchKey(obj) {
     let ans = [];
     let keysArray = Object.keys(obj);
     keysArray.forEach(val => {
-        if (val.includes(sessionKey)) ans.push(val);
+        sessionKey.forEach(sessionVal => {
+            if (val.includes(sessionVal)) ans.push(val);
+        })
     })
     return ans;
 }
@@ -33,7 +43,7 @@ function searchKey(obj) {
 if (sessionKey) {
     (
         async function () {
-            res.innerHTML = `Result : ${sessionKey}`;
+            res.innerHTML = `Result : ${userInp}`;
             let response = await fetch('../assets/searchKeyowrds/search.json');
             let data = await response.json();
             let firstKey = searchKey(data);
@@ -58,3 +68,9 @@ if (sessionKey) {
 else {
     createFailedNode();
 }
+
+//back logic
+let home = document.querySelector('.response img');
+home.addEventListener('click', e => {
+    window.history.back();
+})
