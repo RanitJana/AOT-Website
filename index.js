d7304062307a6d217a6f40724945d0835df38c99
Ranit Jana <ranitjana100@gmail.com>
2024-06-03 01:53:48 +0530

    add backend

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = require('./app.js');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
