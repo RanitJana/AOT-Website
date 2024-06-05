178dacbf59f4a36c03ffe8faf508b707b097f1f7
Ranit Jana <ranitjana100@gmail.com>
2024-06-05 16:30:14 +0530

    add

--- routes/serveOurData.specialRoute.js
+++ routes/serveOurData.specialRoute.js
@@ -5,12 +5,23 @@ const route = express.Router();
 const userSchema = require('../models/student.model.js');
 
 route.get('/getDataStudent', async (req, res) => {
-    try{
-
-        const jsonData = await userSchema.find().sort({ roll: 1 });
-        res.json(jsonData);
+    try {
+        if (req.get('Referer')) {
+            // The request was likely made via a link or redirection
+            const jsonData = await userSchema.find().sort({ roll: 1 });
+            const filteredData = jsonData.map(item => ({
+                roll: item.roll,
+                fullName: item.fullName,
+                semMarks: item.semMarks
+            }));
+            console.log(filteredData);
+            res.json(filteredData);
+        } else {
+            // The request was likely made by manually entering the URL
+            res.status(403).json({ error: 'Unauthorized access' });
+        }
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({ error: 'Error fetching data' });
     }
 })
