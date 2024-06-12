ea7d32c8f9d8da8981843d75835e1416aa90edc7
Ranit Jana <ranitjana100@gmail.com>
2024-06-13 00:13:07 +0530

    add department in chart

--- public/javaScripts/studentDetails.js
+++ public/javaScripts/studentDetails.js
@@ -1,16 +1,10 @@
 let btns = document.querySelectorAll('.editButton');
 const inputs = document.querySelectorAll('.form-group input');
-const select = document.querySelectorAll('select');
 btns[0].addEventListener('click', e => {
     inputs.forEach(input => {
         if (input.getAttribute('id') != 'admissionYear' && input.getAttribute('id') != 'department' && input.getAttribute('id') != 'universityRollNo')
             input.disabled = false;
     });
-    select.forEach(sel => {
-        sel.disabled = false;
-    })
-    select[0].style.backgroundColor = 'white';
-    select[0].style.border = '1px solid gray';
     btns[1].style.display = 'flex';
     btns[0].style.display = 'none';
 })
